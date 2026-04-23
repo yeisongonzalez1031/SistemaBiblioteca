4ec8a2c9251b496a0327489866fce9106e54c29f
dpuertasg <dpuertasg@unicartagena.edu.co>
2026-04-23 17:55:14 -0500

    juan P

--- src/main/java/com/mycompany/sistemabiblioteca/SistemaBiblioteca.java
+++ src/main/java/com/mycompany/sistemabiblioteca/SistemaBiblioteca.java
@@ -11,7 +11,7 @@ package com.mycompany.sistemabiblioteca;
 public class SistemaBiblioteca {
 
     public static void main(String[] args) {
-        
+       System.out.println("galleta a 500");
         
     }
 }
