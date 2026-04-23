9935cf5c4b75c6af9d18a8b82aea9779d1e8c3c9
JuanP-dev100 <jgiraldog@unicartagena.edu.co>
2026-04-23 19:50:05 -0400

    Update SistemaBiblioteca.java

--- src/main/java/vista/SistemaBiblioteca.java
+++ src/main/java/vista/SistemaBiblioteca.java
@@ -16,16 +16,18 @@ import java.util.Scanner;
 public class SistemaBiblioteca {
 
     public static void main(String[] args) {
+
+       System.out.println("galleta a 500");
        System.out.println("===== SISTEMA DE GESTIÓN DE BIBLIOTECA=====");
-       System.out.println("1. CREAR NUEVA CATEGORIA");
-       System.out.println("2. AGREGAR NUEVO LIBRO");
-       System.out.println("3. AGREGAR NUEVO LIBRO");
-       System.out.println("4. REALIZAR PRESTAMO");
-       System.out.println("5. DEVOLVER LIBRO PRESTADO");
-       System.out.println("VER LIBROS DISPONIBLES");
-       System.out.println("7. VER DETALLER DEL AUTOR");
-       System.out.println("8. VER LIBROS DE UNA CATEGORIA");
-       System.out.println("9. VER MULTAS ACUMULADAS");
+       System.out.println("1.  CREAR NUEVA CATEGORIA");
+       System.out.println("2.  AGREGAR NUEVO LIBRO");
+       System.out.println("3.  AGREGAR NUEVO LIBRO");
+       System.out.println("4.  REALIZAR PRESTAMO");
+       System.out.println("5.  DEVOLVER LIBRO PRESTADO");
+       System.out.println("6.  VER LIBROS DISPONIBLES");
+       System.out.println("7.  VER DETALLER DEL AUTOR");
+       System.out.println("8.  VER LIBROS DE UNA CATEGORIA");
+       System.out.println("9.  VER MULTAS ACUMULADAS");
        System.out.println("10. VER ESTADISTICAS");
        System.out.println("11. SALIR");
        
