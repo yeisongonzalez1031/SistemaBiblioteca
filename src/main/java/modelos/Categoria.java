dc9a864c13bf6703df6641e9fb6ba7c8639af3d0
yeisongonzalez1031 <ygonzalezm2@unicartagena.edu.co>
2026-04-23 18:11:53 -0500

    mambo

--- src/main/java/modelos/Categoria.java
+++ src/main/java/modelos/Categoria.java
@@ -4,10 +4,37 @@
  */
 package modelos;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ESTUDIANTE
  */
 public class Categoria {
     
+   private final String id;
+   private final String nombre; 
+   private ArrayList<Libro> libros;
+   
+   public Categoria(){
+   }
+   
+   public Categoria(String id, String nombre, ArrayList<Libro> libros){
+   this.id = id;
+   this.libros = libros;
+   this.nombre = nombre;
+   
+   }
+
+    public ArrayList<Libro> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(ArrayList<Libro> libros) {
+        this.libros = libros;
+    }
+   
+   
+   
+   
 }
