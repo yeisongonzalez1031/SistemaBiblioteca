2ef39277ee22c373fdf3f58f15096cfdc0f11c6a
yeisongonzalez1031 <ygonzalezm2@unicartagena.edu.co>
2026-04-23 17:46:16 -0500

    inicio

---
+++ src/main/java/com/mycompany/sistemabiblioteca/SistemaBiblioteca.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemabiblioteca;
+
+/**
+ *
+ * @author ESCRITORIO
+ */
+public class SistemaBiblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        hola perras
+    }
+}
