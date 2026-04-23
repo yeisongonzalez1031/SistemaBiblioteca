c9a67325d54b94d05491da5cb084acad0a2b9f57
dpuertasg <dpuertasg@unicartagena.edu.co>
2026-04-23 18:31:15 -0500

    PUSHEADA EXTREMA

--- src/main/java/vista/SistemaBiblioteca.java
+++ src/main/java/vista/SistemaBiblioteca.java
@@ -3,6 +3,11 @@
  */
 
 package vista;
+import modelos.Autor;
+import modelos.Biblioteca;
+import modelos.Categoria;
+import modelos.Libro;
+import modelos.Prestamo;
 
 /**
  *
@@ -12,8 +17,17 @@ public class SistemaBiblioteca {
 
     public static void main(String[] args) {
        System.out.println("galleta a 500");
-        /*
-       private int edad;
-       */
+       System.out.println("===== SISTEMA DE GESTIÓN DE BIBLIOTECA=====");
+       System.out.println("1. CREAR NUEVA CATEGORIA");
+       System.out.println("2. AGREGAR NUEVO LIBRO");
+       System.out.println("3. AGREGAR NUEVO LIBRO");
+       System.out.println("4. REALIZAR PRESTAMO");
+       System.out.println("5. DEVOLVER LIBRO PRESTADO");
+       System.out.println("VER LIBROS DISPONIBLES");
+       System.out.println("7. VER DETALLER DEL AUTOR");
+       System.out.println("8. VER LIBROS DE UNA CATEGORIA");
+       System.out.println("9. VER MULTAS ACUMULADAS");
+       System.out.println("10. VER ESTADISTICAS");
+       System.out.println("11. SALIR");
     }
 }
