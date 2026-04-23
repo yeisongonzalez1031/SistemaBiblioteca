7274c59c82f40139e7193ee7e3bd40536c9fb36c
yeisongonzalez1031 <ygonzalezm2@unicartagena.edu.co>
2026-04-23 18:27:21 -0500

    pushueando

--- src/main/java/modelos/Libro.java
+++ src/main/java/modelos/Libro.java
@@ -13,6 +13,33 @@ public class Libro {
     private final int isbn = 123456;
     private final String titulo = "cien anios de seriedad";
     private Autor autor;
+    private Categoria categoria;
+    
+    public Libro(){
+    }
+
+    public Libro(Autor autor, Categoria categoria) {
+        this.autor = autor;
+        this.categoria= categoria;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+    
+    
     
     
     
