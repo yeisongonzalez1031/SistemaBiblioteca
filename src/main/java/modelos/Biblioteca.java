2328d979e5b85ffdc2e2a73ba16cd889ed5713b0
JuanP-dev100 <jgiraldog@unicartagena.edu.co>
2026-04-23 19:50:00 -0400

    pusheando

--- src/main/java/modelos/Biblioteca.java
+++ src/main/java/modelos/Biblioteca.java
@@ -15,5 +15,40 @@ public class Biblioteca {
     private ArrayList<Categoria> categorias;
     private ArrayList<Autor> autores;
     private ArrayList<Prestamo> prestamos;
+
+    public Biblioteca() {
+    }
+
+    public Biblioteca(ArrayList<Categoria> categorias, ArrayList<Autor> autores, ArrayList<Prestamo> prestamos) {
+        this.categorias = categorias;
+        this.autores = autores;
+        this.prestamos = prestamos;
+    }
+
+    public ArrayList<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(ArrayList<Categoria> categorias) {
+        this.categorias = categorias;
+    }
+
+    public ArrayList<Autor> getAutores() {
+        return autores;
+    }
+
+    public void setAutores(ArrayList<Autor> autores) {
+        this.autores = autores;
+    }
+
+    public ArrayList<Prestamo> getPrestamos() {
+        return prestamos;
+    }
+
+    public void setPrestamos(ArrayList<Prestamo> prestamos) {
+        this.prestamos = prestamos;
+    }
  
+    
+    
 }
