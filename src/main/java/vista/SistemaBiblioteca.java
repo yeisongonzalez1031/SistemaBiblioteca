0f34099a5618dbcc355f3a072bfa49161d1ced33
dpuertasg <dpuertasg@unicartagena.edu.co>
2026-04-24 12:58:50 -0500

    cambiaron esta nota para ganar algebra

--- src/main/java/vista/SistemaBiblioteca.java
+++ src/main/java/vista/SistemaBiblioteca.java
@@ -34,7 +34,10 @@ public class SistemaBiblioteca {
        System.out.println("10. VER ESTADISTICAS");
        System.out.println("11. SALIR");
        respuesta = Leer.nextInt();
-     
+      //lista de autores
+      List<Autor> autores = new ArrayList<>();
+      ArrayList<Libro> libros = new ArrayList<>(); //para poder agregar libros
+      List<Categoria> categorias = new ArrayList<>();
           switch(respuesta){
        
            case 1:{
@@ -48,22 +51,26 @@ public class SistemaBiblioteca {
                //datos libro
                System.out.println("=================================");
                int x = 0; 
-               ArrayList<Libro> libros = new ArrayList<>(); //para poder usar en libros
+               
+               
+               //creacion de nueva categoria
                Categoria categoria = new Categoria(idC, libros , nombreC);//se declara despues para pasarle lista vacia
+               
+               categorias.add(categoria); // añadir categorias a una lista, por si acaso
                System.out.println("Necesitamos los datos de los libros que va a agregar a dicha categoria");
                do{
-                   System.out.println("digite el isbn del libro: ");
+                   System.out.println("Digite el isbn del libro: ");
                int isbn = Leer.nextInt();
                Leer.nextLine();//impiar buffer
                
                //datos autor
-               System.out.println("Necsitamos lo datos el autor");
-               System.out.println("Digite elnombre del autor del libro: ");
+               System.out.println("Necesitamos lo datos el autor");
+               System.out.println("Digite el nombre del autor del libro: ");
                String nombreA = Leer.nextLine();
                System.out.println("Digite el id del autor "+ nombreA + ":");
                int idA = Leer.nextInt();// id Autor
                Leer.nextLine(); //limpiar buffer
-               System.out.println("DIigte la nacionalidad del autor: ");
+               System.out.println("Diigte la nacionalidad del autor: ");
                String pais = Leer.nextLine();
                System.out.print("Digite año de nacimiento del autor: ");
                int anio = Leer.nextInt();
@@ -71,6 +78,7 @@ public class SistemaBiblioteca {
                
                //creacion de autor
                Autor autor = new Autor(nombreA, pais, anio, idA);
+               autores.add(autor);//agregar autor a la lista
                //continuacion datos libro
                System.out.println("Digite el titulo del libro asociado al autor " + nombreA + ":");
                String tituloL = Leer.nextLine();
@@ -92,16 +100,103 @@ public class SistemaBiblioteca {
                }
                 }while(x != 1 && x != 0);
                }while(x != 1);
-               
-               //creacion de nueva categoria
               
            break;
            }
            case 2:{
+               //datos autor
+               System.out.println("Digite el nombre del autor que desea: ");
+               String nombreA = Leer.nextLine();
+               System.out.println("Digite el id del autor "+ nombreA + ":");
+               int idA = Leer.nextInt();// id Autor
+               Leer.nextLine(); //limpiar buffer
+               System.out.println("Diigte la nacionalidad del autor: ");
+               String pais = Leer.nextLine();
+               System.out.print("Digite año de nacimiento del autor: ");
+               int anio = Leer.nextInt();
+               Leer.nextLine();
+               
+               //creacion de autor
+               Autor autor = new Autor(nombreA, pais, anio, idA);
                break;
            }
-       
+           case 3: {
+    int x = 0;
+    boolean encontrado = false;
+    Autor autorBuscado = null;
+
+    System.out.println("Digite el isbn del libro: ");
+    int isbn = Leer.nextInt();
+    Leer.nextLine();
+
+    do {
+        System.out.println("el autor ya existe en el sistema? \n0. si\n1. no");
+        x = Leer.nextInt();
+        Leer.nextLine();
+
+        if (x != 0 && x != 1) {
+            System.out.println("No se digito un numero adecuado, por favor repetir");
+        }
+
+    } while (x != 0 && x != 1);
+
+    // no está en el sistema
+    if (x == 1) {
+
+        System.out.println("Necesitamos los datos del autor");
+        System.out.println("Digite el nombre del autor del libro: ");
+        String nombreA = Leer.nextLine();
+
+        System.out.println("Digite el id del autor " + nombreA + ":");
+        int idA = Leer.nextInt();
+        Leer.nextLine();
+
+        System.out.println("Digite la nacionalidad del autor: ");
+        String pais = Leer.nextLine();
+
+        System.out.print("Digite año de nacimiento del autor: ");
+        int anio = Leer.nextInt();
+        Leer.nextLine();
+
+        Autor autor = new Autor(nombreA, pais, anio, idA);
+        autores.add(autor);
+
+        autorBuscado = autor;
+
+    } 
+    // sí está en el sistema
+    else {
+
+        System.out.println("Digite el nombre del autor");
+        String nombre = Leer.nextLine();
+
+        for (int i = 0; i < autores.size(); i++) {
+            if (nombre.equals(autores.get(i).getNombre())) {
+                System.out.println("autor encontrado");
+                autorBuscado = autores.get(i);
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("autor no encontrado");
+        }
     }
-      }while(respuesta != 11);
+
+    if (encontrado || x == 1) {
+        System.out.println("Digite el titulo del libro asociado al autor " 
+                + autorBuscado.getNombre() + ":");
+        String tituloL = Leer.nextLine();
+
+        Libro libro = new Libro(autorBuscado, categoria, isbn, tituloL);
+        libros.add(libro);
+    }
+
+    break;
+}
+               
+}
+}while(respuesta != 11);
 }
 }
